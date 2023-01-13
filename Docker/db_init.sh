14d6760175d59590dec890eb69f69aeba63e2409
Pestap <90579181+Pestap@users.noreply.github.com>
2023-01-14 00:58:30 +0100

    Changed from building an image to pulling from dockerhub

--- Docker/db_init.sh
+++ Docker/db_init.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-docker cp ./prestashop.sql  MariaDB:/tmp/prestashop.sql
-docker cp ./db_load.sh  MariaDB:/tmp/db_load.sh
-docker exec -it MariaDB ./tmp/db_load.sh
-docker exec -it MariaDB rm /tmp/prestashop.sql
-docker exec -it MariaDB rm /tmp/db_load.sh
+docker cp ./prestashop.sql  harzypotterzy_mariadb:/tmp/prestashop.sql
+docker cp ./db_load.sh  harzypotterzy_mariadb:/tmp/db_load.sh
+
+docker exec -it harzypotterzy_mariadb chmod 777 //tmp//db_load.sh
+docker exec -it harzypotterzy_mariadb chmod 777 //tmp//prestashop.sql
+
+docker exec -it harzypotterzy_mariadb //bin//sh //tmp//db_load.sh
+docker exec -it harzypotterzy_mariadb rm //tmp//prestashop.sql
+docker exec -it harzypotterzy_mariadb rm //tmp//db_load.sh
+sleep 10
--- Docker/db_load.sh
+++ Docker/db_load.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-DB_ROOT_PASSWORD=Asseco#123
+DB_ROOT_PASSWORD=root
 DB_USER=harzypotterzy
 DB_PASSWORD=harzypotterzy
 DB_NAME=harzypotterzy_db
+SHOP_URL=localhost:17257
+SHOP_URL_SSL=localhost:17258
 
 mysql -p${DB_ROOT_PASSWORD} -e "DROP DATABASE IF EXISTS ${DB_NAME};"
 mysql -p${DB_ROOT_PASSWORD} -e "CREATE DATABASE ${DB_NAME};"
-mysql -p${DB_ROOT_PASSWORD} -e "USER ${DB_NAME};"
-mysql -p${DB_ROOT_PASSWORD} -e "CREATE OR REPLACE USER '${DB_USER}'@localhost IDENTIFIED BY '${DB_PASSWORD}';"
-mysql -p${DB_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON '${DB_NAME}'.* TO '${DB_USER}'@localhost;"
+mysql -p${DB_ROOT_PASSWORD} -e "CREATE OR REPLACE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+mysql -p${DB_ROOT_PASSWORD} -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
 mysql -p${DB_ROOT_PASSWORD} -e "FLUSH PRIVILEGES;"
 
 mysql -u ${DB_USER} -p${DB_PASSWORD} ${DB_NAME} < /tmp/prestashop.sql
+
+mysql -u ${DB_USER} -p${DB_PASSWORD} ${DB_NAME} -e "UPDATE ps_shop_url SET domain='${SHOP_URL}', domain_ssl='${SHOP_URL_SSL}' WHERE id_shop_url=1;"
